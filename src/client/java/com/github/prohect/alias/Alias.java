db6df7b8175ea21a79f5802281ff3218531956ff
Prohect <prohect@foxmail.com>
2025-08-09 14:33:45 +0800

    add TODO list

--- src/client/java/com/github/prohect/alias/Alias.java
+++ src/client/java/com/github/prohect/alias/Alias.java
@@ -1,5 +1,16 @@
 package com.github.prohect.alias;
 
+/*
+TODO:
+ forwardKey;
+ leftKey;
+ backKey;
+ rightKey;
+ jumpKey;
+ sneakKey;
+ sprintKey;
+ dropKey;
+ */
 public interface Alias {
     void run(String args);
 }
