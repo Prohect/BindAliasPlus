d387eccbfd7d63f7fef8fa221f888f75f06c910c
Prohect <prohect@foxmail.com>
2026-06-27 17:29:06 +0800

    feat: add localSay alias for 1.21_1.21.3

---
+++ src/client/java/com/github/prohect/alias/builtinAlias/LocalSayAlias.java
@@ -0,0 +1,19 @@
+package com.github.prohect.alias.builtinAlias;
+
+import com.github.prohect.alias.BuiltinAliasWithGreedyStringArgs;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.text.Text;
+
+public class LocalSayAlias extends BuiltinAliasWithGreedyStringArgs<LocalSayAlias> {
+
+    public LocalSayAlias() {
+        super("localSay");
+    }
+
+    @Override
+    public LocalSayAlias run(String args) {
+        if (MinecraftClient.getInstance().player == null) return this;
+        MinecraftClient.getInstance().inGameHud.getChatHud().addMessage(Text.literal(args));
+        return this;
+    }
+}
