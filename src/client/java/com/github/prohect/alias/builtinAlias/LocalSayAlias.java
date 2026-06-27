9f5e77f41752825ab8c68a480602037ba5ef1472
Prohect <prohect@foxmail.com>
2026-06-27 17:18:33 +0800

    feat: add localSay alias, fix SwapSlotAlias screen guard, bump to 1.2.6

---
+++ src/client/java/com/github/prohect/alias/builtinAlias/LocalSayAlias.java
@@ -0,0 +1,19 @@
+package com.github.prohect.alias.builtinAlias;
+
+import com.github.prohect.alias.BuiltinAliasWithGreedyStringArgs;
+import net.minecraft.client.Minecraft;
+import net.minecraft.network.chat.Component;
+
+public class LocalSayAlias extends BuiltinAliasWithGreedyStringArgs<LocalSayAlias> {
+
+    public LocalSayAlias() {
+        super("localSay");
+    }
+
+    @Override
+    public LocalSayAlias run(String args) {
+        if (Minecraft.getInstance().player == null) return this;
+        Minecraft.getInstance().gui.hud.getChat().addClientSystemMessage(Component.literal(args));
+        return this;
+    }
+}
