16b905929c6a2469c14b1d8927903dda831b0cd2
Prohect <prohect@foxmail.com>
2026-07-03 20:26:57 +0800

    Guard UseAlias and PickItemAlias from text input screens

--- src/client/java/com/github/prohect/alias/builtinAlias/PickItemAlias.java
+++ src/client/java/com/github/prohect/alias/builtinAlias/PickItemAlias.java
@@ -1,5 +1,6 @@
 package com.github.prohect.alias.builtinAlias;
 
+import com.github.prohect.alias.Alias;
 import com.github.prohect.alias.BuiltinAliasWithoutArgs;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.option.KeyBinding;
@@ -19,6 +20,7 @@ public class PickItemAlias extends BuiltinAliasWithoutArgs<PickItemAlias> {
     public PickItemAlias run(String args) {
         MinecraftClient mc = MinecraftClient.getInstance();
         if (mc.player == null) return this;
+        if (Alias.isUnderTextInputScreen()) return this;
         KeyBinding pickKey = mc.options.pickItemKey;
         pickKey.setPressed(true);
         pickKey.timesPressed++;
