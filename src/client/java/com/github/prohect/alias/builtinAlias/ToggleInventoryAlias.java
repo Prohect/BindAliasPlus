7bb530823463ec0dfb1fe241e7c636f011d82e74
Prohect <prohect@foxmail.com>
2026-07-03 18:57:53 +0800

    Implement toggleinventory alias

---
+++ src/client/java/com/github/prohect/alias/builtinAlias/ToggleInventoryAlias.java
@@ -0,0 +1,32 @@
+package com.github.prohect.alias.builtinAlias;
+
+import com.github.prohect.alias.Alias;
+import com.github.prohect.alias.BuiltinAliasWithoutArgs;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.screen.ingame.InventoryScreen;
+
+public class ToggleInventoryAlias
+    extends BuiltinAliasWithoutArgs<ToggleInventoryAlias>
+{
+
+    public ToggleInventoryAlias() {
+        super("toggleinventory");
+    }
+
+    /**
+     * Toggle the inventory screen: open if closed, close if open.
+     */
+    @Override
+    public ToggleInventoryAlias run(String args) {
+        // cancel open event from text input screen
+        if (Alias.isUnderTextInputScreen()) return this;
+        MinecraftClient mc = MinecraftClient.getInstance();
+        if (mc.player == null) return this;
+        if (Alias.isInContainerScreen()) {
+            Alias.getCurrentScreen().close();
+        } else if (!Alias.isUnderAnyScreen()) {
+            mc.setScreen(new InventoryScreen(mc.player));
+        }
+        return this;
+    }
+}
