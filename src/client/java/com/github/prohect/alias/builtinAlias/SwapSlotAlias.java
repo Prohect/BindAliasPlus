516af66fe5dd9ee46d535c88922ab9063313eb33
Prohect <prohect@foxmail.com>
2026-07-24 04:35:40 +0800

    fix: SwapSlotAlias use getSelectedSlot() on 1.21.9+

--- src/client/java/com/github/prohect/alias/builtinAlias/SwapSlotAlias.java
+++ src/client/java/com/github/prohect/alias/builtinAlias/SwapSlotAlias.java
@@ -46,7 +46,7 @@ public class SwapSlotAlias extends BuiltinAliasWithArgs<SwapSlotAlias> {
             BindAliasPlusClient.LOGGER.warn("{}[switchSlot]Inventory is null", BindAliasPlusClient.tickPrefix());
             return this;
         }
-        int selectedSlot = inventory.selectedSlot;
+        int selectedSlot = inventory.getSelectedSlot();
         ClientPlayNetworkHandler networkHandler =
             minecraftClient.getNetworkHandler();
         if (networkHandler == null) {
